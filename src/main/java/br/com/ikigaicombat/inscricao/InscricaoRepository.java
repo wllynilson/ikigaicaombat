6313d5acff466165b6f9b1ba26339a6e14185faa
Wllynilson <wllynilson@gmail.com>
2025-06-02 15:24:20 -0300

    add annotation on repository inscriptions

--- src/main/java/br/com/ikigaicombat/inscricao/InscricaoRepository.java
+++ src/main/java/br/com/ikigaicombat/inscricao/InscricaoRepository.java
@@ -2,8 +2,9 @@ package br.com.ikigaicombat.inscricao;
 
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface InscricaoRepository extends JpaRepository<Inscricao, UUID> {
 
 }

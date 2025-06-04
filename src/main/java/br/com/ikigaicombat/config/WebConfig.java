aeb445ec10af527954185556d66d5832b3fc11d6
Wllynilson <wllynilson@gmail.com>
2025-06-04 15:12:33 -0300

    update localhost

--- src/main/java/br/com/ikigaicombat/config/WebConfig.java
+++ src/main/java/br/com/ikigaicombat/config/WebConfig.java
@@ -1,6 +1,5 @@
 package br.com.ikigaicombat.config; // Ou um pacote de configuração adequado
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // Aplica a todas as rotas da sua API
                 .allowedOrigins(
                         "http://localhost:63342", // Se você usa o servidor embutido do IntelliJ
-                        "http://127.0.0.1:63342", // Alternativa para localhost
+                        "http://127.0.0.1:5500", // Alternativa para localhost
                         "http://localhost:8000",  // Exemplo se você usar 'python -m http.server 8000' localmente
                         "https://www.ikigaicombat.com.br", // SEU DOMÍNIO DE PRODUÇÃO
                         "null" // Para testes locais abrindo o arquivo file:///... (use com cautela)
